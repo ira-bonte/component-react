8bc96777586a2471828d0dd42ef874c98ec4d3ae
Iradukunda Bonte <irabonte123@gmail.com>
2023-08-11 21:08:35 -0700

    react work

---
+++ src/component/Form/Form.js
@@ -0,0 +1,22 @@
+import React from "react";
+import App from "../../App";
+function Form(){
+    return (
+        <center>
+    <form>
+    <fieldset>
+    <legend>User Login</legend>
+    <label>Username</label><br/>
+    <input type="text" value="Iradukunda Bonte"/><br/>
+    <label>E-mail</label><br/>
+    <input type="email" value="irabonte123@gmail.com"/><br/>
+    <label>Phone Number</label><br/>
+    <input type="number" value="0792332012"/><br/>
+    <label>Residence</label><br/>
+    <input type="text" value="Gasabo"/><br/>
+    </fieldset>
+    </form>
+    </center>
+    )
+}
+    export default Form
